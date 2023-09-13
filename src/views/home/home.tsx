0ff966725e8de8e0b44c287bb93919945d1e5ab2
Pedro La Rosa D <pedrolarosad@gmail.com>
2023-09-13 23:52:54 +0200

    chore(home.tsx): format code for better readability feat(home.tsx): add support for displaying service tracks fix(home.tsx): use unique key for service tracks mapping chore(home.tsx): add TODO comment for future improvement

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -1,11 +1,59 @@
-import { Cover, Stack } from "@rosepath/react-layouts";
+import {
+	Cover,
+	Stack,
+	Center,
+	PadBox,
+	Cluster,
+	Grid,
+} from "@rosepath/react-layouts";
 import { Header } from "../../components/header";
 import { Footer } from "../../components/footer";
+import { ServiceTrack } from "../../models/service-track";
 
-function Home() {
+interface HomeProps {
+	serviceTracks: ServiceTrack[];
+}
+
+function Home(props: HomeProps) {
 	return (
 		<Cover as={Stack} topComponent={<Header />} bottomComponent={<Footer />}>
-			HOme
+			<Center as={Stack} maxWidth={"768px"}>
+				<PadBox as="section">
+					{props.serviceTracks.map((serviceTrack) => (
+						// TODO:
+						// Since the service doesn't provide id per record
+						// I had to use the combination of the first name and the last name
+						<Stack key={serviceTrack.firstName} gutter="size0">
+							<Cluster as="p">
+								<p>Customer: </p>
+								<p>
+									{serviceTrack.firstName} {serviceTrack.lastName}
+								</p>
+							</Cluster>
+							<Cluster as="p">
+								<p>Vehicle: </p>
+								<p>
+									{serviceTrack.brand} {serviceTrack.model} {serviceTrack.year}
+								</p>
+							</Cluster>
+
+							<Grid>
+								{serviceTrack?.services.map((service) => (
+									<Stack key={`${service.code}${service.date}`}>
+										<Cluster as="p">
+											<p>service: </p>
+											<p>
+												{service.code} {service.date} {service.cost}
+											</p>
+										</Cluster>
+										<PadBox>{service.description}</PadBox>
+									</Stack>
+								))}
+							</Grid>
+						</Stack>
+					))}
+				</PadBox>
+			</Center>
 		</Cover>
 	);
 }
