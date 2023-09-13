a0759db69a856796d88b3778a672164ba9ef224c
Pedro La Rosa D <pedrolarosad@gmail.com>
2023-09-13 23:52:17 +0200

    feat(router.tsx): add error boundary to handle errors in route components feat(router.tsx): add loader function to fetch data for home route feat(router.tsx): pass fetched data to Home component in home route

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,24 +1,37 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, useLoaderData } from "react-router-dom";
 import { homePath, newServicePath } from "./routes";
 import { Home } from "../views/home";
 import { NewService } from "../views/new-service";
 import { NotFound } from "../views/not-found";
+import { CarTrackerService } from "../services/car-tracker";
+import { ServiceTrack } from "../models/service-track";
+import { RootErrorBoundary } from "./error-boundary/root-error-boundary";
+import { adaptCarServiceToServiceTrack } from "../models/adapters/in/adaptCarServiceToServiceTrack";
 
 const router = createBrowserRouter([
 	{
 		path: homePath,
+		async loader() {
+			const carTracker = new CarTrackerService();
+			const { tracks } = await carTracker.getCarServiceTracks();
+			return tracks.map(adaptCarServiceToServiceTrack);
+		},
+		errorElement: <RootErrorBoundary />,
 		Component() {
-			return <Home />;
+			const data = useLoaderData() as ServiceTrack[];
+			return <Home serviceTracks={data} />;
 		},
 	},
 	{
 		path: newServicePath,
+		errorElement: <RootErrorBoundary />,
 		Component() {
 			return <NewService />;
 		},
 	},
 	{
 		path: "*",
+		errorElement: <RootErrorBoundary />,
 		Component() {
 			return <NotFound />;
 		},
