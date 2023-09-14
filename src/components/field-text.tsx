70389b9f46e5efab021f9626754ac0598ad1b887
Pedro La Rosa D <pedrolarosad@gmail.com>
2023-09-15 01:53:44 +0200

    feat(field-text.tsx): add FieldText component for rendering a text input field
    The FieldText component is a reusable component that renders a text input field. It takes several props including label, placeholder, type, id, defaultValue, currency, and validate. It uses the react-final-form library to handle form validation and rendering. The component renders a label, input field, and error message if there is a validation error.

---
+++ src/components/field-text.tsx
@@ -0,0 +1,36 @@
+import { Field } from "react-final-form";
+
+const FieldText = (props: {
+	label: string;
+	placeholder?: string;
+	type: string;
+	id: string;
+	defaultValue?: string;
+	currency?: string;
+	validate?: (value: string) => string | undefined;
+}) => (
+	<Field
+		name={props.id}
+		validate={props.validate}
+		render={({ input, meta }) => (
+			<div className="field">
+				<label className="label" htmlFor={props.id}>
+					{props.label}
+				</label>
+				<div className="control is-expanded ">
+					<input
+						id={props.id}
+						className="input"
+						{...input}
+						type={props.type}
+						placeholder={props.placeholder}
+						defaultValue={props.defaultValue}
+					/>
+				</div>
+				{meta.touched && meta.error && <span>{meta.error}</span>}
+			</div>
+		)}
+	/>
+);
+
+export { FieldText };
