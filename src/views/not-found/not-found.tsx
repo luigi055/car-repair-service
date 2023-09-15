44c32657ff7c5b0fc8466b68f716462df1d788fa
Pedro La Rosa D <pedrolarosad@gmail.com>
2023-09-15 23:20:24 +0200

    refactor(not-found.tsx): Update NotFound component to use new layout components and add a link to go back home

--- src/views/not-found/not-found.tsx
+++ src/views/not-found/not-found.tsx
@@ -1,11 +1,30 @@
-import { Cover } from "@rosepath/react-layouts";
+import { Center, Stack, PadBox, Cluster, Cover } from "@rosepath/react-layouts";
 import { Header } from "../../components/header";
 import { Footer } from "../../components/footer";
+import { Link } from "react-router-dom";
+import { homePath } from "../../routes";
 
 function NotFound() {
 	return (
 		<Cover as={"main"} topComponent={<Header />} bottomComponent={<Footer />}>
-			Not Found
+			<Center>
+				<PadBox className="box has-background-primary" padding="size4">
+					<PadBox className="box has-background-white">
+						<Stack as={Center} hasTextCentered>
+							<h2 className="title is-5 has-text-black-ter">
+								Ops! This page doesn't exist
+							</h2>
+							<Stack as={Center} hasTextCentered gutter="size4">
+								<Cluster as={Center} gutter="size4">
+									<Link to={homePath} className="button is-link is-light">
+										Take me back home
+									</Link>
+								</Cluster>
+							</Stack>
+						</Stack>
+					</PadBox>
+				</PadBox>
+			</Center>
 		</Cover>
 	);
 }
