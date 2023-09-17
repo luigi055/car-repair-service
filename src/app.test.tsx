f2c7cc52dbaf01fb7cb253659352d4771ef80769
Pedro La Rosa D <pedrolarosad@gmail.com>
2023-09-17 15:29:24 +0200

    test(app.test.tsx): remove unused imports test(app.test.tsx): add test for showing all initial records from the api test(app.test.tsx): add test for showing all elements of a service track information card test(app.test.tsx): add test for showing the correct footer information

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -1,5 +1,5 @@
 import { byRole } from "testing-library-selector";
-import { screen, render, waitFor, within } from "@testing-library/react";
+import { render, waitFor, within } from "@testing-library/react";
 import { it, expect, describe } from "vitest";
 import App from "./app";
 
@@ -8,6 +8,7 @@ const ui = {
 	newServiceLink: byRole("link", { name: "Register new service" }),
 	serviceTrackListSection: byRole("heading", { name: "Service track List:" }),
 	serviceTrackCard: byRole("article", { name: "service track information" }),
+	footerInformation: byRole("contentinfo", { name: "" }),
 };
 
 describe("Testing the Home page", () => {
@@ -22,55 +23,63 @@ describe("Testing the Home page", () => {
 		expect(ui.newServiceLink.get()).toBeDefined();
 	});
 
-	describe("Testing home page", () => {
-		it("should show all the initial records coming from the api", async () => {
-			render(<App />);
+	it("should show all the initial records coming from the api", async () => {
+		render(<App />);
 
-			await waitFor(() => {
-				expect(ui.serviceTrackListSection.get()).toBeDefined();
-			});
-
-			expect(ui.serviceTrackCard.getAll()).toHaveLength(4);
+		await waitFor(() => {
+			expect(ui.serviceTrackListSection.get()).toBeDefined();
 		});
 
-		it("should show all elements of a service track information card", async () => {
-			render(<App />);
+		expect(ui.serviceTrackCard.getAll()).toHaveLength(4);
+	});
 
-			await waitFor(() => {
-				expect(ui.serviceTrackListSection.get()).toBeDefined();
-			});
+	it("should show all elements of a service track information card", async () => {
+		render(<App />);
 
-			// the third article is going to be used as model since it contains more than one services
-			const serviceInformationArticle = within(ui.serviceTrackCard.getAll()[2]);
-
-			expect(serviceInformationArticle.getByText("Ralph Benson"));
-			expect(serviceInformationArticle.getByText("Honda Civic 2014"));
-
-			const services = serviceInformationArticle.getAllByRole("region", {
-				name: "service information",
-			});
-
-			expect(services).toHaveLength(2);
-
-			const firstServiceInformation = within(services[0]);
-
-			expect(firstServiceInformation.getByText("Service: 1001")).toBeDefined();
-			expect(firstServiceInformation.getByText("Cost: $36.42")).toBeDefined();
-			expect(firstServiceInformation.getByText("March 13, 2019")).toBeDefined();
-			expect(
-				firstServiceInformation.getByText("Description: Oil change")
-			).toBeDefined();
-
-			const secondServiceInformation = within(services[1]);
-
-			expect(secondServiceInformation.getByText("Service: 1003")).toBeDefined();
-			expect(secondServiceInformation.getByText("Cost: $206.14")).toBeDefined();
-			expect(
-				secondServiceInformation.getByText("March 13, 2019")
-			).toBeDefined();
-			expect(
-				secondServiceInformation.getByText("Description: A/C recharge")
-			).toBeDefined();
+		await waitFor(() => {
+			expect(ui.serviceTrackListSection.get()).toBeDefined();
 		});
+
+		// the third article is going to be used as model since it contains more than one services
+		const serviceInformationArticle = within(ui.serviceTrackCard.getAll()[2]);
+
+		expect(serviceInformationArticle.getByText("Ralph Benson"));
+		expect(serviceInformationArticle.getByText("Honda Civic 2014"));
+
+		const services = serviceInformationArticle.getAllByRole("region", {
+			name: "service information",
+		});
+
+		expect(services).toHaveLength(2);
+
+		const firstServiceInformation = within(services[0]);
+
+		expect(firstServiceInformation.getByText("Service: 1001")).toBeDefined();
+		expect(firstServiceInformation.getByText("Cost: $36.42")).toBeDefined();
+		expect(firstServiceInformation.getByText("March 13, 2019")).toBeDefined();
+		expect(
+			firstServiceInformation.getByText("Description: Oil change")
+		).toBeDefined();
+
+		const secondServiceInformation = within(services[1]);
+
+		expect(secondServiceInformation.getByText("Service: 1003")).toBeDefined();
+		expect(secondServiceInformation.getByText("Cost: $206.14")).toBeDefined();
+		expect(secondServiceInformation.getByText("March 13, 2019")).toBeDefined();
+		expect(
+			secondServiceInformation.getByText("Description: A/C recharge")
+		).toBeDefined();
+	});
+
+	it("should show the correct footer information", async () => {
+		render(<App />);
+
+		await waitFor(() => {
+			expect(ui.serviceTrackListSection.get()).toBeDefined();
+		});
+
+		expect(ui.footerInformation.get().textContent).toBe(
+			"Made by Pedro La Rosa"
+		);
 	});
 });
